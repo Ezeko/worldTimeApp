e27673d36cf7d4b027e44880095b6ed401a3bf1f
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-09-08 12:01:23 +0100

    remove safe area widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -27,65 +27,63 @@ class _HomeState extends State<Home> {
         data['isDayTime'] ? Colors.purple[200] : Colors.purple[700];
     return Scaffold(
       backgroundColor: bgColor,
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/$bgImage'),
-                fit: BoxFit.cover,
-          )),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 200.0, 0, 0),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 8.0),
-                  RaisedButton.icon(
-                    color: iconColor,
-                    onPressed: () async {
-                      dynamic result =
-                          await Navigator.pushNamed(context, '/choose');
-                      //print(result);
-                      setState(() {
-                        data = result;
-                      });
-                      //print(data);
-                    },
-                    icon: Icon(
-                      Icons.edit_location,
-                      color: Colors.deepPurple[50],
-                    ),
-                    label: Text(
-                      'Choose location',
-                      style: TextStyle(color: Colors.deepPurple[50]),
-                    ),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/$bgImage'),
+              fit: BoxFit.cover,
+        )),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 200.0, 0, 0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 8.0),
+                RaisedButton.icon(
+                  color: iconColor,
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/choose');
+                    //print(result);
+                    setState(() {
+                      data = result;
+                    });
+                    //print(data);
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.deepPurple[50],
                   ),
-                  SizedBox(height: 8.0),
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/${data['flag']}'),
-                    radius: 38.0,
+                  label: Text(
+                    'Choose location',
+                    style: TextStyle(color: Colors.deepPurple[50]),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        data['location'],
-                        style: TextStyle(
-                          fontSize: 28.0,
-                          letterSpacing: 2.0,
-                          color: textColor,
-                        ),
+                ),
+                SizedBox(height: 8.0),
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/${data['flag']}'),
+                  radius: 38.0,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: textColor,
                       ),
-                      Text(
-                        data['time'],
-                        style: TextStyle(
-                          fontSize: 38.0,
-                          color: textColor,
-                        ),
+                    ),
+                    Text(
+                      data['time'],
+                      style: TextStyle(
+                        fontSize: 38.0,
+                        color: textColor,
                       ),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
