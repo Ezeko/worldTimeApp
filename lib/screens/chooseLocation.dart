9728fc90dff49221ee53a2b765974adfcf657b46
Ezekiel ADEJOBI <ezeko2017@gmail.com>
2020-09-09 08:59:21 +0100

    remove unwanted import

--- lib/screens/chooseLocation.dart
+++ lib/screens/chooseLocation.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:load/load.dart';
 import 'package:worldTime/services/worldTimeService.dart';
